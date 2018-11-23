4217049ef48c5acba7d0562352b279d0e516a41b
vexx32 <32407840+vexx32@users.noreply.github.com>
2018-11-23 16:04:41 -0500

    Move StopWords to a new file, adjust split behaviour. Increment version for publish

--- PSWordCloud/Public/New-WordCloud.ps1
+++ PSWordCloud/Public/New-WordCloud.ps1
@@ -237,24 +237,8 @@ function New-WordCloud {
         $MaxUniqueWords = 100
     )
     begin {
-        $ExcludedWords = @(
-            'a', 'all', 'also', 'am', 'an', 'and', 'any', 'are', 'as',
-            'at', 'be', 'but', 'by', 'can', 'can''t',
-            'cannot', 'com', 'could', 'couldn''t', 'did', 'didn''t', 'do', 'does', 'doesn''t', 'doing', 'don''t', 'down',
-            'each', 'else', 'ever', 'few', 'for', 'from', 'get', 'had', 'hadn''t', 'has', 'hasn''t',
-            'have', 'haven''t', 'having', 'he', 'he''d', 'he''ll', 'he''s', 'her', 'here', 'here''s', 'hers',
-            'herself', 'him', 'himself', 'his', 'how', 'how''s', 'http', 'i', 'i''d', 'i''ll', 'i''m', 'i''ve', 'if',
-            'in', 'into', 'is', 'isn''t', 'it', 'it''s', 'its', 'just', 'k', 'let''s', 'like', 'me', 'more', 'most',
-            'mustn''t', 'my', 'myself', 'no', 'nor', 'not', 'of', 'off', 'on', 'once', 'only', 'or', 'other',
-            'ought', 'our', 'ours', 'ourselves', 'out', 'over', 'own', 'r', 'same', 'shall', 'shan''t', 'she', 'she''d',
-            'she''ll', 'she''s', 'since', 'so', 'some', 'such', 'than', 'that', 'that''s', 'the',
-            'their', 'theirs', 'them', 'then', 'there', 'there''s', 'these', 'they', 'they''d',
-            'they''ll', 'they''re', 'they''ve', 'this', 'those', 'through', 'to', 'too', 'under', 'until', 'up', 'very', 'was',
-            'wasn''t', 'we', 'we''d', 'we''ll', 'we''re', 'we''ve', 'were', 'weren''t', 'what', 'what''s', 'when', 'when''s',
-            'where', 'where''s', 'which', 'while', 'who', 'who''s', 'whom', 'why', 'why''s', 'with', 'won''t', 'would',
-            'wouldn''t', 'www', 'you', 'you''d', 'you''ll', 'you''re', 'you''ve', 'your', 'yours', 'yourself', 'yourselves'
-        ) -join '|'
-        $SplitChars = " `n.,`"?!{}[]:()`“`”™" -as [char[]]
+        $ExcludedWords = (Get-Content "$script:ModuleRoot/Data/StopWords.txt") -join '|'
+        $SplitChars = " `n.,`"?!{}[]:()`“`”™*#%^&+=" -as [char[]]
         $ColorIndex = 0
         $RadialDistance = 0
 
