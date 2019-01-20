61d440ef7bf9083918933182d5d102d6742ee648
vexx32 <32407840+vexx32@users.noreply.github.com>
2019-01-20 01:26:41 -0500

    :wrench: Fix import issue on PS 5.1
    :memo: Add-Type before importing functions

--- PSWordCloud/PSWordCloud.psm1
+++ PSWordCloud/PSWordCloud.psm1
@@ -1,14 +1,6 @@
 ﻿$script:ModuleRoot = $PSScriptRoot
 Write-Verbose "PSWordCloud module root: $script:ModuleRoot"
 
-$PublicFunctions = Get-ChildItem "$script:ModuleRoot\Public"
-$PrivateFunctions = Get-ChildItem "$script:ModuleRoot\Private"
-
-foreach ($Function in @($PublicFunctions) + @($PrivateFunctions)) {
-    Write-Verbose "Importing functions from file: [$($Function.Name)]"
-    . $Function.Fullname
-}
-
 # PowerShell Core uses System.Drawing.Common assembly instead of System.Drawing
 if ($PSEdition -eq 'Core') {
     Write-Verbose 'Importing necessary types.'
@@ -17,6 +9,14 @@ if ($PSEdition -eq 'Core') {
 else {
     Write-Verbose 'Importing necessary types.'
     Add-Type -AssemblyName 'System.Drawing'
+}
+
+$PublicFunctions = Get-ChildItem "$script:ModuleRoot\Public"
+$PrivateFunctions = Get-ChildItem "$script:ModuleRoot\Private"
+
+foreach ($Function in @($PublicFunctions) + @($PrivateFunctions)) {
+    Write-Verbose "Importing functions from file: [$($Function.Name)]"
+    . $Function.Fullname
 }
 
 Export-ModuleMember -Function $PublicFunctions.BaseName
