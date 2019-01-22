fcb72ee067ff617e8ae1c03302f3a4942d9eea9a
Joel <32407840+vexx32@users.noreply.github.com>
2019-01-22 17:30:37 -0500

    :construction: real EOD commit - getting stopwords in

--- Cmdlet/NewWordCloudCommand.cs
+++ Cmdlet/NewWordCloudCommand.cs
@@ -31,6 +31,21 @@ namespace PSWordCloud
 
         private List<string> _inputCache = new List<string>(256);
         private List<Task<string[]>> _taskCache = new List<Task<string[]>>();
+        private static string[] _stopWords = {
+            "a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be",
+            "because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't",
+            "did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further",
+            "had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's",
+            "hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is",
+            "isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of",
+            "off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't",
+            "she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their",
+            "theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've",
+            "this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're",
+            "we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's",
+            "whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your",
+            "yours","yourself","yourselves"
+        }
 
         protected override void BeginProcessing()
         {
@@ -50,20 +65,26 @@ namespace PSWordCloud
 
         protected override void ProcessRecord()
         {
+            string[] text;
             if (MyInvocation.ExpectingInput)
             {
-                var inputObject = new[] { InputObject.BaseObject as string };
+                text = new[] { InputObject.BaseObject as string };
             }
             else
             {
-                var inputObject = InputObject.BaseObject as string[];
-                if (inputObject == null)
+                text = InputObject.BaseObject as string[];
+                if (text == null)
                 {
-                    inputObject = new[] { InputObject.BaseObject as string };
+                    text = new[] { InputObject.BaseObject as string };
                 }
             }
 
+            _inputCache.AddRange(text);
 
+            if (!MyInvocation.ExpectingInput || _inputCache.Count >= 250)
+            {
+                // Kick off async job
+            }
         }
 
         protected override void EndProcessing()
@@ -71,6 +92,13 @@ namespace PSWordCloud
 
         }
 
+        private async Task<string[]> SubdivideTextAsync(string[] lines)
+        {
+            List<string> words = new List<string>(lines.Length);
 
+
+
+            return words.ToArray();
+        }
     }
 }
