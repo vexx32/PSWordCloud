20f30e6871c41fa07cb1e62790b8338e65fb22e3
Joel <32407840+vexx32@users.noreply.github.com>
2019-02-05 16:47:09 -0500

    :sparkles: :recycle: Add extension methods :sparkles: Add IntersectsPath extension method
    :recycle: cleanup adding to occupied region with ext method

--- Cmdlet/src/WCUtils.cs
+++ Cmdlet/src/WCUtils.cs
@@ -22,6 +22,51 @@ namespace PSWordCloud
             return (float)(degrees * Math.PI / 180);
         }
 
+        public static bool SetPath(this SKRegion region, SKPath path, bool usePathBounds)
+        {
+            if (usePathBounds && path.GetBounds(out SKRect bounds))
+            {
+                using (SKRegion clip = new SKRegion())
+                {
+                    clip.SetRect(SKRectI.Ceiling(bounds));
+                    return region.SetPath(path, clip);
+                }
+            }
+            else
+            {
+                return region.SetPath(path);
+            }
+        }
+
+        public static bool Op(this SKRegion region, SKPath path, SKRegionOperation operation)
+        {
+            using (SKRegion pathRegion = new SKRegion())
+            {
+                pathRegion.SetPath(path, true);
+                return region.Op(pathRegion, operation);
+            }
+        }
+
+        public static bool IntersectsPath(this SKRegion region, SKPath path)
+        {
+            if (region.Bounds.IsEmpty)
+            {
+                return false;
+            }
+
+            using (SKRegion pathRegion = new SKRegion())
+            {
+                pathRegion.SetPath(path, region);
+
+                if (region.Intersects(pathRegion))
+                {
+                    return true;
+                }
+
+                return false;
+            }
+        }
+
         internal static readonly ReadOnlyDictionary<string, SKColor> ColorLibrary =
             new ReadOnlyDictionary<string, SKColor>(typeof(SKColors)
             .GetFields(BindingFlags.Static | BindingFlags.Public)
