25c32fa7e6bf969d1fbeb987a2d154cfd2fb8cb2
vexx32 <32407840+vexx32@users.noreply.github.com>
2019-02-06 23:52:41 -0500

    :construction: remove unneeded ext method

--- Cmdlet/src/WCUtils.cs
+++ Cmdlet/src/WCUtils.cs
@@ -25,19 +25,6 @@ namespace PSWordCloud
             return (float)(degrees * Math.PI / 180);
         }
 
-        public static void Shuffle<T>(this IList<T> list, Random rng)
-        {
-            int n = list.Count;
-            while (n > 1)
-            {
-                n--;
-                int k = rng.Next(n + 1);
-                T value = list[k];
-                list[k] = list[n];
-                list[n] = value;
-            }
-        }
-
         /// <summary>
         /// Checks if any part of the rectangle lies outside the region's bounds.
         /// </summary>
