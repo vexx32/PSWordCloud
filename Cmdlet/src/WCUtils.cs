68c18d39ded26c43e560a8eb32098f3da6802f64
vexx32 <32407840+vexx32@users.noreply.github.com>
2019-02-08 01:01:49 -0500

    :wrench: Increase stroke width to be noticeable. :memo: And other misc. tweaks to size, scaling, and strokes

--- Cmdlet/src/WCUtils.cs
+++ Cmdlet/src/WCUtils.cs
@@ -55,7 +55,7 @@ namespace PSWordCloud
         public static void NextWord(this SKPaint brush, float wordSize, float strokeWidth, SKColor color)
         {
             brush.TextSize = wordSize;
-            brush.StrokeWidth = wordSize * strokeWidth / 100;
+            brush.StrokeWidth = wordSize * strokeWidth / 25;
             brush.IsStroke = false;
             brush.IsVerticalText = false;
             brush.Color = color;
