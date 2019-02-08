d40e8ef7448bd4a56313a4adad29337c88a58ea6
vexx32 <32407840+vexx32@users.noreply.github.com>
2019-02-07 23:57:58 -0500

    :sparkles: Add Shuffle method

--- Cmdlet/src/WCUtils.cs
+++ Cmdlet/src/WCUtils.cs
@@ -25,6 +25,18 @@ namespace PSWordCloud
             return (float)(degrees * Math.PI / 180);
         }
 
+        public static void Shuffle<T>(this Random rng, T[] array)
+        {
+            int n = array.Length;
+            while (n > 1)
+            {
+                int k = rng.Next(n--);
+                T temp = array[n];
+                array[n] = array[k];
+                array[k] = temp;
+            }
+        }
+
         /// <summary>
         /// Checks if any part of the rectangle lies outside the region's bounds.
         /// </summary>
