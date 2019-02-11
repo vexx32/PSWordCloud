623d5b08d916d74f273dc29719c8d7e8db7c0758
vexx32 <32407840+vexx32@users.noreply.github.com>
2019-02-11 00:16:09 -0500

    :construction: Refactor PSM1, Restructure for Xplat :construction: Rework PSM1 to work with all platforms and new structures
    :construction: moving things around again, oops
    
    :memo: Update gitignore for build dir
    
    :sparkles: add build script
    
    :construction: Move reference in test file to new location
    
    :construction: tweak location references in build script

---
+++ Module/PSWordCloud.psm1
@@ -0,0 +1,23 @@
+﻿$PlatformFolder = switch ($true) {
+    $IsWindows {
+        if ([IntPtr]::Size -eq 8) {
+            "win-x64"
+        }
+        else {
+            "win-x86"
+        }
+    }
+    $IsMacOS {
+        "osx"
+    }
+    $IsLinux {
+        "linux-x64"
+    }
+}
+
+$SkiaDllPath = Join-Path -Path $PSScriptRoot -ChildPath $PlatformFolder "SkiaSharp.dll"
+
+Add-Type -Path $SkiaDllPath
+Import-Module  "$PSScriptRoot\PSWordCloudCmdlet.dll"
+
+Export-ModuleMember -Cmdlet "New-WordCloud"
