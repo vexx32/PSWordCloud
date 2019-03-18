7242e08c66b5cf5129fdadf3af177ec336e6f1d6
Joel <32407840+vexx32@users.noreply.github.com>
2019-03-18 12:39:20 -0400

    :wrench: Fix WinPS compatibility!

--- Module/PSWordCloud.psm1
+++ Module/PSWordCloud.psm1
@@ -8,11 +8,16 @@
     $IsLinux {
         "linux-x64"
     }
+    default {
+        # Windows PowerShell
+        if ([Environment]::Is64BitProcess) { "win-x64" } else { "win-x86" }
+    }
 }
 
-$SkiaDllPath = Join-Path -Path $PSScriptRoot -ChildPath $PlatformFolder "SkiaSharp.dll"
+$SkiaDllPath = Join-Path -Path $PSScriptRoot -ChildPath $PlatformFolder |
+    Join-Path -ChildPath "SkiaSharp.dll"
 
 Add-Type -Path $SkiaDllPath
-Import-Module  "$PSScriptRoot\PSWordCloudCmdlet.dll"
+Import-Module  "$PSScriptRoot/PSWordCloudCmdlet.dll"
 
 Export-ModuleMember -Cmdlet "New-WordCloud"
