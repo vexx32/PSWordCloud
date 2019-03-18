c41672597dfa1fdd20df150c532f7c9f96ec4355
Joel Sallow (/u/ta11ow) <32407840+vexx32@users.noreply.github.com>
2019-03-18 12:45:17 -0400

    :wrench: :recycle: Fix Windows PowerShell Compatibility (#23)
    * :wrench: Fix WinPS compatibility!
    
    * :recycle: Use Join-Path for safety

--- Module/PSWordCloud.psm1
+++ Module/PSWordCloud.psm1
@@ -8,11 +8,18 @@
     $IsLinux {
         "linux-x64"
     }
+    default {
+        # Windows PowerShell
+        if ([Environment]::Is64BitProcess) { "win-x64" } else { "win-x86" }
+    }
 }
 
-$SkiaDllPath = Join-Path -Path $PSScriptRoot -ChildPath $PlatformFolder "SkiaSharp.dll"
+$SkiaDllPath = Join-Path -Path $PSScriptRoot -ChildPath $PlatformFolder |
+    Join-Path -ChildPath "SkiaSharp.dll"
 
 Add-Type -Path $SkiaDllPath
-Import-Module  "$PSScriptRoot\PSWordCloudCmdlet.dll"
+
+$ModuleDllPath = Join-Path -Path $PSScriptRoot -ChildPath "PSWordCloudCmdlet.dll"
+Import-Module $ModuleDllPath
 
 Export-ModuleMember -Cmdlet "New-WordCloud"
