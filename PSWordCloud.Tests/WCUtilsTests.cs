978608c015dcafd38d93ddaad1ac6e5dd7c8ba01
Joel <32407840+vexx32@users.noreply.github.com>
2019-03-20 13:15:19 -0400

    :white_check_mark: Add tests for utils

--- PSWordCloud.Tests/WCUtilsTests.cs
+++ PSWordCloud.Tests/WCUtilsTests.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Linq;
 using Xunit;
 using PSWordCloud;
+using SkiaSharp;
 
 namespace PSWordCloud.Tests
 {
@@ -12,6 +14,26 @@ namespace PSWordCloud.Tests
             //Assert.Equal(KnownResult, TestResult);
 
             Assert.Equal(0, ((float)0).ToRadians());
+        }
+
+        [Fact]
+        public void Test_Multiply()
+        {
+            var point = new SKPoint(1, 2);
+
+            Assert.Equal(point.Multiply(5), new SKPoint(5, 10));
+            Assert.Equal(point.Multiply(1.5f), new SKPoint(1.5f, 3));
+        }
+
+        [Fact]
+        public void Test_Shuffle()
+        {
+            var collection = new[] { 1, 2, 3, 4, 5 };
+            var original = new[] { 1, 2, 3, 4, 5 };
+            var rng = new Random();
+            rng.Shuffle(collection);
+
+            Assert.False(Enumerable.SequenceEqual(original, collection));
         }
     }
 }
