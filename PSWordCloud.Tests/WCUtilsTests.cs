022e400eb1ac30e322a65030f2319473b7f7086c
vexx32 <32407840+vexx32@users.noreply.github.com>
2019-03-21 22:42:49 -0400

    :white_check_mark: Add tests for ext methods

--- PSWordCloud.Tests/WCUtilsTests.cs
+++ PSWordCloud.Tests/WCUtilsTests.cs
@@ -35,5 +35,42 @@ namespace PSWordCloud.Tests
 
             Assert.False(Enumerable.SequenceEqual(original, collection));
         }
+
+        [Fact]
+        public void Test_FallsOutside()
+        {
+            var rect = new SKRect(0, 0, 10, 10);
+            var region = new SKRegion();
+
+            region.SetRect(new SKRectI(-5, -5, 5, 5));
+
+            Assert.True(rect.FallsOutside(region));
+
+            rect = new SKRect(-5, -5, 5, 5);
+            Assert.False(rect.FallsOutside(region));
+        }
+
+        [Fact]
+        public void Test_NextWord()
+        {
+            var brush = new SKPaint();
+            brush.NextWord(2.4f, 1.8f, SKColors.Beige);
+
+            Assert.Equal(brush.TextSize, 2.4f);
+            Assert.False(brush.IsStroke);
+            Assert.Equal(brush.Style, SKPaintStyle.StrokeAndFill);
+            Assert.Equal(brush.StrokeWidth, 1.8f * 1.8f * NewWordCloudCommand.STROKE_BASE_SCALE);
+            Assert.False(brush.IsVerticalText);
+            Assert.Equal(brush.Color, SKColors.Beige);
+        }
+
+        [Fact]
+        public void Test_SortValue()
+        {
+            var color = SKColors.Blue;
+            var expectedValue = 100 * (1 - 0.5) / (1 - 100);
+
+            Assert.Equal(color.SortValue(1), expectedValue);
+        }
     }
 }
