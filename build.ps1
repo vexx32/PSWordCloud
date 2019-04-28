1407f98e8b9c40c40eafef1fe4f258cc293f2960
vexx32 <32407840+vexx32@users.noreply.github.com>
2019-04-28 01:55:12 -0400

    :construction: Only copy one SkiaSharp.dll to built module

--- build.ps1
+++ build.ps1
@@ -35,21 +35,18 @@ $SupportedPlatforms = "win-x64", "win-x86", "linux-x64", "osx"
 $ModulePath = Join-Path $OutputPath -ChildPath "PSWordCloud"
 New-Item -Path $ModulePath -ItemType Directory | Out-Null
 
-# Copy the main module DLL to final module directory
+# Copy the main module DLLs to final module directory
 Copy-Item -Path "$OutputPath/bin/PSWordCloudCmdlet.dll" -Destination $ModulePath
+Copy-Item -Path "$OutputPath/bin/SkiaSharp.dll" -Destination $ModulePath
 
-# Get the main Skia DLL
-$SkiaDLL = Get-Item -Path "$OutputPath/bin/SkiaSharp.dll"
-
-# Get platform-specific runtime library folders for Skia
+# Copy platform-specific runtime library folders for Skia
 $RuntimeFolders = Get-ChildItem -Path "$OutputPath/bin/runtimes" |
     Where-Object Name -in $SupportedPlatforms
 
 $RuntimeFolders | ForEach-Object {
     $OutputDirectory = New-Item -ItemType Directory -Path "$ModulePath/$($_.Name)"
     Get-ChildItem -Path $_.FullName -Recurse -Include *.dylib, *.dll, *.so |
-        Copy-Item -Destination $OutputDirectory.FullName -PassThru |
-        ForEach-Object { Copy-Item -Path $SkiaDLL.FullName -Destination $OutputDirectory.FullName }
+        Copy-Item -Destination $OutputDirectory.FullName
 }
 
 Split-Path -Path $ProjectFile -Parent |
