a504c47c1c9773ffa96964d82ca215feabd70cef
vexx32 <32407840+vexx32@users.noreply.github.com>
2019-04-28 01:52:48 -0400

    :wrench: Add runtime folder to DllLoadPaths

--- Module/PSWordCloud.psm1
+++ Module/PSWordCloud.psm1
@@ -1,25 +1,30 @@
-﻿$PlatformFolder = switch ($true) {
+﻿Add-Type -TypeDefinition @"
+    using System.Runtime.InteropServices;
+
+    public class DllLoadPath
+    {
+        [DllImport("kernel32", CharSet=CharSet.Unicode)]
+        public static extern int AddDllDirectory(string NewDirectory);
+    }
+"@
+
+$PlatformFolder = switch ($true) {
     $IsWindows {
         if ([Environment]::Is64BitProcess) { "win-x64" } else { "win-x86" }
     }
-    $IsMacOS {
-        "osx"
-    }
-    $IsLinux {
-        "linux-x64"
-    }
+    $IsMacOS { "osx" }
+    $IsLinux { "linux-x64" }
     default {
         # Windows PowerShell
         if ([Environment]::Is64BitProcess) { "win-x64" } else { "win-x86" }
     }
 }
 
-$SkiaDllPath = Join-Path -Path $PSScriptRoot -ChildPath $PlatformFolder |
-    Join-Path -ChildPath "SkiaSharp.dll"
+$NativeRuntimeFolder = Join-Path -Path $PSScriptRoot -ChildPath $PlatformFolder
+[DllLoadPath]::AddDllDirectory($NativeRuntimeFolder)
 
+$SkiaDllPath = Join-Path -Path $PSScriptRoot -ChildPath "SkiaSharp.dll"
 Add-Type -Path $SkiaDllPath
 
 $ModuleDllPath = Join-Path -Path $PSScriptRoot -ChildPath "PSWordCloudCmdlet.dll"
 Import-Module $ModuleDllPath
-
-Export-ModuleMember -Cmdlet "New-WordCloud"
