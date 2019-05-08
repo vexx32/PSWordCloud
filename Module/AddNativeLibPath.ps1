cb2a858c2fb1b6431d71c93c86cf248b9dac951d
Joel <32407840+vexx32@users.noreply.github.com>
2019-05-08 12:24:09 -0400

    :construction: Messy but hopefully OK xplat fix

--- Module/AddNativeLibPath.ps1
+++ Module/AddNativeLibPath.ps1
@@ -1,24 +1,42 @@
-﻿Add-Type -TypeDefinition @"
-    using System.Runtime.InteropServices;
+﻿switch ($true) {
+    $IsMacOS {
+        $NativeRuntimeFolder = Join-Path -Path $PSScriptRoot -ChildPath "osx"
+        $ConfigPath = Join-Path -Path $NativeRuntimeFolder -ChildPath "libSkiaSharp.dylib.config"
+        @"
+<configuration>
+  <dllmap dll="libSkiaSharp" target="libSkiaSharp.dylib" />
+</configuration>
+"@ | Set-Content -Path $ConfigPath
 
-    public class DllLoadPath
-    {
-        [DllImport("kernel32", CharSet=CharSet.Unicode)]
-        public static extern int SetDllDirectory(string NewDirectory);
+        $env:DYLD_FRAMEWORK_PATH = "{0}{1}{2}" -f @(
+            $NativeRuntimeFolder
+            [IO.Path]::PathSeparator
+            $env:DYLD_FRAMEWORK_PATH
+        )
     }
-"@
-
-$PlatformFolder = switch ($true) {
-    $IsWindows {
-        if ([Environment]::Is64BitProcess) { "win-x64" } else { "win-x86" }
+    $IsLinux {
+        $NativeRuntimeFolder = Join-Path -Path $PSScriptRoot -ChildPath "linux-x64"
+        $env:LD_LIBRARY_PATH = "{0}{1}{2}" -f @(
+            $NativeRuntimeFolder
+            [IO.Path]::PathSeparator
+            $env:LD_LIBRARY_PATH
+        )
     }
-    $IsMacOS { "osx" }
-    $IsLinux { "linux-x64" }
     default {
-        # Windows PowerShell
-        if ([Environment]::Is64BitProcess) { "win-x64" } else { "win-x86" }
+        # Windows PowerShell / PS Core on Windows
+        Add-Type -TypeDefinition @"
+            using System.Runtime.InteropServices;
+
+            public class DllLoadPath
+            {
+                [DllImport("kernel32", CharSet=CharSet.Unicode)]
+                public static extern int SetDllDirectory(string NewDirectory);
+            }
+"@
+        $PlatformFolder = if ([Environment]::Is64BitProcess) { "win-x64" } else { "win-x86" }
+        $NativeRuntimeFolder = Join-Path -Path $PSScriptRoot -ChildPath $PlatformFolder
+
+        [DllLoadPath]::SetDllDirectory($NativeRuntimeFolder)
     }
 }
 
-$NativeRuntimeFolder = Join-Path -Path $PSScriptRoot -ChildPath $PlatformFolder
-[DllLoadPath]::SetDllDirectory($NativeRuntimeFolder)
