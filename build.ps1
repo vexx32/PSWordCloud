13f21e6b15e8c64a170566877f81bda7ed43ffbb
Joel Sallow <32407840+vexx32@users.noreply.github.com>
2020-03-12 22:01:06 -0400

    :bug: :wrench: Fix the build!

--- build.ps1
+++ build.ps1
@@ -22,32 +22,33 @@ if (Test-Path -Path $OutputPath) {
     Remove-Item -Recurse -Path $OutputPath
 }
 
-$Dotnet = Start-Process -NoNewWindow -PassThru -FilePath 'dotnet' -ArgumentList @(
-    'publish'
-    "-c $Channel"
-    "-o $(Join-Path -Path $OutputPath -ChildPath "bin")"
-    $ProjectFile
-)
-
-$Dotnet.WaitForExit()
+$ModulePath = Join-Path $OutputPath -ChildPath 'PSWordCloud'
 
 $SupportedPlatforms = "win-x64", "win-x86", "linux-x64", "osx"
-$ModulePath = Join-Path $OutputPath -ChildPath "PSWordCloud"
-New-Item -Path $ModulePath -ItemType Directory | Out-Null
+
+foreach ($rid in $SupportedPlatforms) {
+    $binPath = Join-Path -Path $OutputPath -ChildPath "bin"
+    $Dotnet = Start-Process -NoNewWindow -PassThru -FilePath 'dotnet' -ArgumentList @(
+        'publish'
+        "-c $Channel"
+        "-o $binPath"
+        $ProjectFile
+        "-r $rid"
+    )
+    $Dotnet | Wait-Process
+
+    $nativeLib = Join-Path $OutputPath -ChildPath 'bin' |
+        Get-ChildItem -Recurse -File -Filter '*libSkiaSharp*'
+    $destinationPath = Join-Path $ModulePath -ChildPath $rid |
+        New-Item -Path { $_ } -ItemType Directory |
+        Select-Object -ExpandProperty FullName
+
+    $nativeLib | Move-Item -Destination $destinationPath -Force
+}
 
 # Copy the main module DLLs to final module directory
 Copy-Item -Path "$OutputPath/bin/PSWordCloudCmdlet.dll" -Destination $ModulePath
 Copy-Item -Path "$OutputPath/bin/SkiaSharp.dll" -Destination $ModulePath
-
-# Copy platform-specific runtime library folders for Skia
-$RuntimeFolders = Get-ChildItem -Path "$OutputPath/bin/runtimes" |
-    Where-Object Name -in $SupportedPlatforms
-
-$RuntimeFolders | ForEach-Object {
-    $OutputDirectory = New-Item -ItemType Directory -Path "$ModulePath/$($_.Name)"
-    Get-ChildItem -Path $_.FullName -Recurse -Include *.dylib, *.dll, *.so |
-        Copy-Item -Destination $OutputDirectory.FullName
-}
 
 Split-Path -Path $ProjectFile -Parent |
     Get-ChildItem -Recurse -Include "*.ps*1" |
