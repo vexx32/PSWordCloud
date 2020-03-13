6ea30269a082332a351a2215c9ee324bfe2be246
Joel Francis <32407840+vexx32@users.noreply.github.com>
2020-03-13 11:35:54 -0400

    :rocket: wat

--- build/Register-FileSystemRepository.ps1
+++ build/Register-FileSystemRepository.ps1
@@ -10,12 +10,15 @@ param(
 )
 
 $RepositoryFolder = if (-not (Test-Path $Path)) {
+    Write-Host "Folder does not exist; creating '$Path'"
     New-Item -ItemType Directory -Path $Path -Force
 }
 else {
+    Write-Host "Getting existing folder '$Path'"
     Get-Item -Path $Path
 }
 
+Write-Host 'Registering Filesystem Repository'
 $Params = @{
     Name                 = $Name
     SourceLocation       = $RepositoryFolder.FullName
