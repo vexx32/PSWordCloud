819ddffad489fbb05de5891ec76986d5d79bbe5c
Joel Sallow <32407840+vexx32@users.noreply.github.com>
2020-03-14 17:35:43 -0400

    :memo: Tidy logging in build file

--- build/Build-Module.ps1
+++ build/Build-Module.ps1
@@ -15,13 +15,20 @@ param(
     $ProjectFile = (Join-Path -Path "$PSScriptRoot/.." -ChildPath "Module" "PSWordCloudCmdlet.csproj")
 )
 
+$Line = '-' * 50
+
 $PSVersionTable | Out-String | Write-Host
+
+Write-Host $Line
+Write-Host "Setup"
+Write-Host $Line
 
 Write-Host "Importing PlatyPS"
 Import-Module PlatyPS
 
 if (Test-Path -Path $OutputPath) {
     Write-Host "Cleaning up '$OutputPath'"
+
     Get-ChildItem -Path $OutputPath -Directory |
         Where-Object Name -in 'bin', 'PSWordCloud' |
         Remove-Item -Recurse
@@ -31,6 +38,9 @@ $ModulePath = Join-Path $OutputPath -ChildPath 'PSWordCloud'
 
 $SupportedPlatforms = "win-x64", "win-x86", "linux-x64", "osx"
 
+Write-Host $Line
+Write-Host "Compiling module with dotnet publish"
+Write-Host $Line
 foreach ($rid in $SupportedPlatforms) {
     $binPath = Join-Path -Path $OutputPath -ChildPath "bin"
     if (Test-Path $binPath) {
@@ -38,7 +48,7 @@ foreach ($rid in $SupportedPlatforms) {
     }
 
     Write-Host "Running 'dotnet publish' with RID: $rid"
-    $process = Start-Process -FilePath 'dotnet' -WindowStyle Hidden -PassThru -ArgumentList @(
+    $process = Start-Process -FilePath 'dotnet' -NoNewWindow -Wait -PassThru -ArgumentList @(
         'publish'
         "--configuration $Channel"
         "--output $binPath"
@@ -46,15 +56,8 @@ foreach ($rid in $SupportedPlatforms) {
         "--runtime $rid"
     )
 
-    try {
-        Write-Host "Waiting for dotnet process to exit..."
-        $process.WaitForExit()
-    }
-    catch {
-        Write-Warning "dotnet process errored on WaitForExit()"
-    }
-
     Write-Host "Locating 'libSkiaSharp' file for $rid"
+
     $nativeLib = Join-Path $OutputPath -ChildPath 'bin' |
         Get-ChildItem -Recurse -File -Filter '*libSkiaSharp*'
     $destinationPath = Join-Path $ModulePath -ChildPath $rid |
@@ -65,22 +68,34 @@ foreach ($rid in $SupportedPlatforms) {
     $nativeLib | Move-Item -Destination $destinationPath -Force -PassThru
 }
 
-Write-Host "Copying PSWordCloud and SkiaSharp DLLs to '$ModulePath'"
+Write-Host $Line
+Write-Host "Copying PSWordCloud and SkiaSharp Files to '$ModulePath'"
+Write-Host $Line
+
 Copy-Item -Path "$OutputPath/bin/PSWordCloudCmdlet.dll" -Destination $ModulePath
 Copy-Item -Path "$OutputPath/bin/SkiaSharp.dll" -Destination $ModulePath
 
-Write-Host "Copying PowerShell files to module folder"
+Write-Host "Copying script & metadata files to module folder"
+
 Split-Path -Path $ProjectFile -Parent |
     Get-ChildItem -Recurse -Include "*.ps*1" |
     Copy-Item -Destination $ModulePath
+
+Write-Host $Line
+Write-Host "Generating External Help"
+Write-Host $Line
 
 Write-Host "Importing the built module"
 Import-Module $ModulePath
 
 $DocsPath = Join-Path "$PSScriptRoot/.." -ChildPath "docs"
+
 Write-Host "Updating markdown help files in '$DocsPath'"
+
 Update-MarkdownHelp -Path $DocsPath -AlphabeticParamsOrder
 
 $ExternalHelpPath = Join-Path $ModulePath -ChildPath "en-US"
+
 Write-Host "Generating external help in '$ExternalHelpPath'"
+
 New-ExternalHelp -Path $DocsPath -OutputPath $ExternalHelpPath
