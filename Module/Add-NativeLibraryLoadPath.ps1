57475e19f94ace09b0ca97501c94db485da9df99
Joel Sallow <32407840+vexx32@users.noreply.github.com>
2020-03-14 04:36:29 -0400

    :bug: use custom assemblyloadcontext

--- Module/Add-NativeLibraryLoadPath.ps1
+++ Module/Add-NativeLibraryLoadPath.ps1
@@ -2,56 +2,6 @@ if ($PSVersionTable.PSVersion.Major -ge 7) {
     return
 }
 
-Add-Type -TypeDefinition @"
-using System;
-using System.IO;
-using System.Runtime.InteropServices;
-
-namespace PSWordCloud
-{
-    [Flags]
-    internal enum DLOpenFlags
-    {
-        RTLD_LAZY = 1,
-        RTLD_NOW = 2,
-        RTLD_LOCAL = 4,
-        RTLD_GLOBAL = 8,
-    }
-
-    internal static class UnixNativeMethods
-    {
-        [DllImport("libdl", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
-        public static extern IntPtr dlopen(
-            string dlToOpen,
-            DLOpenFlags flags);
-    }
-
-    internal static class Win32NativeMethods
-    {
-        [DllImport("kernel32", EntryPoint="LoadLibraryW")]
-        public static extern IntPtr LoadLibrary(
-            [InAttribute, MarshalAs(UnmanagedType.LPWStr)] string dllToLoad);
-    }
-
-    public static class NativeMethods
-    {
-        public static IntPtr LibHandle { get; private set; }
-
-        public static void LoadNativeLibrary(string dllPath)
-        {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                LibHandle = Win32NativeMethods.LoadLibrary(dllPath);
-            }
-            else
-            {
-                LibHandle = UnixNativeMethods.dlopen(dllPath, DLOpenFlags.RTLD_NOW);
-            }
-        }
-    }
-}
-"@
-
 $PlatformFolder = switch ($true) {
     $IsWindows {
         if ([Environment]::Is64BitProcess) { "win-x64" } else { "win-x86" }
@@ -65,6 +15,21 @@ $PlatformFolder = switch ($true) {
 }
 
 $NativeRuntimeFolder = Join-Path -Path $PSScriptRoot -ChildPath $PlatformFolder
-$DllPath = Get-Item -Path "$NativeRuntimeFolder/libSkiaSharp.*"
 
-[PSWordCloud.NativeMethods]::LoadNativeLibrary($DllPath.FullName)
+Add-Type -TypeDefinition @"
+using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace PSWordCloud
+{
+    public class NativeLoadContext : AssemblyLoadContext
+    {
+        protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
+        {
+            return LoadUnmanagedDllFromPath(Path.Combine($NativeRuntimeFolder, unmanagedDllName));
+        }
+    }
+}
+"@
