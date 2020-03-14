af6605d409f6828c3200f603fd4bb6979783f996
Joel Sallow <32407840+vexx32@users.noreply.github.com>
2020-03-14 07:42:43 -0400

    :bug: fix filesystem deployment path

--- build/FileSystem/PSWordCloud.psdeploy.ps1
+++ build/FileSystem/PSWordCloud.psdeploy.ps1
@@ -1,6 +1,6 @@
 Deploy Module {
     By PSGalleryModule {
-        FromSource "$env:BUILD_ARTIFACTSTAGINGDIRECTORY/PSWordCloud"
+        FromSource "$PSScriptRoot/../PSWordCloud"
         To FileSystem
         WithOptions @{
             ApiKey = 'FileSystem'
