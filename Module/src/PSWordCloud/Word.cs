c31fd92a5767aef8968858531e55d0e302da214d
Joel Sallow <32407840+vexx32@users.noreply.github.com>
2020-09-24 05:51:28 -0400

    :memo: Add XML comments for Word.cs
    Added XML comments for all internal/public members

--- Module/src/PSWordCloud/Word.cs
+++ Module/src/PSWordCloud/Word.cs
@@ -3,13 +3,28 @@ using SkiaSharp;
 
 namespace PSWordCloud
 {
+    /// <summary>
+    /// Defines the <see cref="Word"/> class, containing the actual string as well as size/scale information and the
+    /// necessary paths required to draw the word in an image.
+    /// </summary>
     internal class Word : IDisposable
     {
+        /// <summary>
+        /// Creates a new instance of the <see cref="Word"/> class.
+        /// </summary>
+        /// <param name="text">The string that comprises the word.</param>
+        /// <param name="relativeSize">The size of this word relative to others in its collection.</param>
         internal Word(string text, float relativeSize)
             : this(text, relativeSize, isFocusWord: false)
         {
         }
 
+        /// <summary>
+        /// Creates a new instance of the <see cref="Word"/> class.
+        /// </summary>
+        /// <param name="text">The string that comprises the word.</param>
+        /// <param name="relativeSize">The size of this word relative to others in its collection.</param>
+        /// <param name="isFocusWord">If true, marks this word as the focus word for the collection.</param>
         internal Word(string text, float relativeSize, bool isFocusWord)
         {
             Text = text;
@@ -17,14 +32,29 @@ namespace PSWordCloud
             IsFocusWord = isFocusWord;
         }
 
-        internal string Text { get; set; }
+        /// <summary>
+        /// Gets the text of the word.
+        /// </summary>
+        internal string Text { get; }
 
-        internal float RelativeSize { get; set; }
+        /// <summary>
+        /// Gets the relative size of the word.
+        /// </summary>
+        internal float RelativeSize { get; }
 
+        /// <summary>
+        /// Gets the fully scaled size of the word.
+        /// </summary>
         internal float ScaledSize { get; private set; }
 
-        internal bool IsFocusWord { get; private set; } = false;
+        /// <summary>
+        /// Gets a value which indicates whether the word should be prominently featured.
+        /// </summary>
+        internal bool IsFocusWord { get; } = false;
 
+        /// <summary>
+        /// Gets or sets the <see cref="SKPath"/> value which determines how it is drawn in the image.
+        /// </summary>
         internal SKPath Path {
             get => _path ??= new SKPath();
             set
@@ -38,6 +68,9 @@ namespace PSWordCloud
             }
         }
 
+        /// <summary>
+        /// Gets or sets the <see cref="SKPath"/> that defines the bubble which will surround the word in the image.
+        /// </summary>
         internal SKPath? Bubble { get; set; } = null;
 
         internal SKRect Bounds {
@@ -45,6 +78,9 @@ namespace PSWordCloud
             set => _bounds = value;
         }
 
+        /// <summary>
+        /// Gets or sets the width of the padding space on each side of the word.
+        /// </summary>
         internal float Padding { get; set; } = 0;
 
         private SKRect? _bounds = null;
@@ -63,8 +99,14 @@ namespace PSWordCloud
             return ScaledSize;
         }
 
+        /// <summary>
+        /// Returns the <see cref="Text"/> of the word.
+        /// </summary>
         public override string ToString() => Text;
 
+    /// <summary>
+    /// Disposes the <see cref="IDisposable"/> managed objects owned by this instance.
+    /// </summary>
         public void Dispose()
         {
             _path?.Dispose();
