64b8203ec12c2b1a97a583693117e9890c6e14f1
Joel Sallow <32407840+vexx32@users.noreply.github.com>
2020-09-24 06:12:42 -0400

    :memo: add XML comments to Ellipse.cs
    Added XML comments for all internal / public members

--- Module/src/PSWordCloud/Ellipse.cs
+++ Module/src/PSWordCloud/Ellipse.cs
@@ -7,22 +7,48 @@ using SkiaSharp;
 
 namespace PSWordCloud
 {
+    /// <summary>
+    /// Defines an ellipse comprised of a collection of points.
+    /// </summary>
     internal class Ellipse
     {
+        /// <summary>
+        /// Gets the list of points that comprises this <see cref="Ellipse"/>.
+        /// </summary>
         internal IReadOnlyList<SKPoint> Points { get => _points; }
 
+        /// <summary>
+        /// Gets the point at the centre of the ellipse.
+        /// </summary>
         internal SKPoint Centre { get; }
 
+        /// <summary>
+        /// Gets the ratio between the two axes of the ellipse.
+        /// </summary>
         internal float AspectRatio { get; }
 
+        /// <summary>
+        /// Gets the radius value used to construct the ellipse.
+        /// </summary>
+        /// <value>If the <see cref="AspectRatio"/> is less than 1, this represents the semi-major axis. Otherwise, this
+        /// represents the semi-minor axis.</value>
         internal float Radius { get; }
 
+        /// <summary>
+        /// Gets the minimum distance between each point on the ellipse.
+        /// </summary>
         internal float PointDistance { get;  }
 
         private readonly List<SKPoint> _points;
-
         private readonly LockingRandom _safeRandom;
 
+        /// <summary>
+        /// Creates a new instance of the <see cref="Ellipse"/> class.
+        /// </summary>
+        /// <param name="radius">One semi-axis of the ellipse.</param>
+        /// <param name="radialStep">The typical distance between two points on the ellipse.</param>
+        /// <param name="image">The image which is used to determine the size of the other axis of the ellipse.</param>
+        /// <param name="randomSeed">A random seed value.</param>
         internal Ellipse(float radius, float radialStep, Image image, int? randomSeed)
         {
             _safeRandom = randomSeed is null
@@ -39,6 +65,12 @@ namespace PSWordCloud
             CalculatePoints();
         }
 
+        /// <summary>
+        /// Creates a new instance of the <see cref="Ellipse"/> class.
+        /// </summary>
+        /// <param name="radius">One semi-axis of the ellipse.</param>
+        /// <param name="radialStep">The typical distance between two points on the ellipse.</param>
+        /// <param name="image">The image which is used to determine the size of the other axis of the ellipse.</param>
         internal Ellipse(float semiMinorAxis, float radialStep, Image image)
             : this(semiMinorAxis, radialStep, image, randomSeed: null)
         {
